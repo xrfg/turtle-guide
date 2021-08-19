6778d7228031b4c075321fe1b35fd8a5356d0dcc
cmi112 <cabdiqanimuuse7@gmail.com>
2021-08-19 14:56:38 +0200

    add css files

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React from "react"
 import './App.css';
+import Footer from "./components/footer/Footer";
+import Header from "./components/pages/Header";
+
 
 function App() {
   return (
     <div className="App">
       <h1>Here is the main app</h1>
+      <Header/>
+    
+      <Footer/>
        
     </div>
   );
--- client/src/components/singup/Singup.js
+++ client/src/components/singup/Singup.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import "../singup/singup.css"
 
 export default function Singup() {
   return (
-    <div>
+    <div className="singup-container">
       <h1>Sing Up</h1>
     </div>
   )
