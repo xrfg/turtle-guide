40929ef5d8504fc4088788400beca0688bf79b43
Marco Vignotto <marcovignotto81@gmail.com>
2021-08-19 13:27:46 +0200

    First server.js ok

--- server.js
+++ server.js
@@ -1,10 +1,63 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
+// Mongo Connection
+
+// PORT
 const PORT = process.env.PORT;
 
+/**
+ * @desc Connect MONGO
+ */
+
+/**
+ * @desc external middleware
+ */
+
+app.use(express.json({ extended: true }));
+
+/**
+ * @desc CORS
+ */
+
+app.use(cors());
+
+/**
+ * @desc Routes
+ */
+
 app.use("/test", (req, res, next) => {
-  res.send("<h1>HI TURTLES</h1>");
+  res.send("<h1>HELLO</h1>");
 });
+
+/**
+ * @desc page not found middleware
+ */
+
+app.use((req, res, next) => {
+  let message = "Page not Found";
+  let status = 404;
+
+  next({ message: message, status: status });
+});
+
+/**
+ * @desc Global erorr middleware
+ */
+
+app.use((err, req, res, next) => {
+  // default 500
+  res.status(err.status || 500).send({
+    success: false,
+    message: "Something went wrong!",
+    status: err.status,
+    error: err.message,
+  });
+});
+
+/**
+ * @desc app listen
+ */
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
