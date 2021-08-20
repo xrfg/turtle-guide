1ca9d60c47df2a6cbee03f0f22667cb0885c67c9
henriquemartins <henrm@riseup.net>
2021-08-20 12:52:02 +0200

    making folder structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
-
-import React from 'react';
+import React from "react";
+import "App.scss";
 
 function App() {
   return (
     <div className="App">
       <h1>My React App</h1>
+      <h2>if the route is GUIDE display Guide wraping component</h2>
     </div>
   );
 }
-
 
 export default App;
---
+++ client/src/Components/Guide/Guide.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function Guide() {
+  // Guide.js wraps all the VISITOR sees
+  return <div>Wraping all the guide</div>;
+}
