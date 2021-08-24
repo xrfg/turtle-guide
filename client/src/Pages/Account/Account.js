e64370945b864b072bf455b21a4c1b41a30034b2
henriquemartins <henrm@riseup.net>
2021-08-24 12:29:47 +0200

    Creating Account Page w/MUI

--- client/src/Pages/Account/Account.js
+++ client/src/Pages/Account/Account.js
@@ -1,19 +1,92 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {
+  Container,
+  Grid,
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+  makeStyles,
+} from "@material-ui/core";
+import { Add } from "@material-ui/icons";
 
-const Account = ({ user }) => {
+const useStyles = makeStyles((theme) => ({
+  card: {
+    marginTop: "10rem",
+    textAlign: "center",
+    position: "relative",
+  },
+  link: { textDecoration: "none", color: "inherit", width: "100%" },
+  btn: { width: "100%" },
+  box: { flexDirection: "column" },
+}));
+
+const Account = (props) => {
+  const classes = useStyles(props);
+
+  // this are only examples of what we would get from the database (events, urls)
+  const events = ["Van Gogh"];
+  const urls = ["van-gogh"];
   return (
-    <div className="container">
-      <div className="row gap-4">
-        <div style={{ height: "10rem" }} className="bg-info col p-2">
-          <Link to="/events">Events</Link>
-        </div>
+    <Container>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+      >
+        <Grid item xs={6}>
+          <Card className={classes.card}>
+            <CardContent>
+              <Typography variant="h3" component="span">
+                Events
+              </Typography>
+            </CardContent>
+            <CardActions className={classes.box}>
+              {events &&
+                events.map((event, i) => {
+                  return (
+                    <Button
+                      className={classes.btn}
+                      variant="text"
+                      color="primary"
+                    >
+                      <Link className={classes.link} to={`/${urls[i]}`}>
+                        {event}
+                      </Link>
+                    </Button>
+                  );
+                })}
 
-        <div className="bg-info col p-2">
-          <Link to="/about-admin">Admin Page</Link>
-        </div>
-      </div>
-    </div>
+              <Button
+                href="/create-event"
+                className={classes.btn}
+                variant="text"
+                color="primary"
+              >
+                <Link className={classes.link} to="/create-event">
+                  <Add />
+                </Link>
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={6}>
+          <Card className={classes.card}>
+            <CardContent>
+              <Link className={classes.link}>
+                <Typography variant="h3" component="span">
+                  About
+                </Typography>
+              </Link>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
