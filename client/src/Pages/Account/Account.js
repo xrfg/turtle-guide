e9d05741c23a4a040933c9728e33e5caadf0efc9
Marco Vignotto <writeme@marcovignotto.com>
2021-08-25 16:44:08 +0200

    Exported and created <ContentBlock />

--- client/src/Pages/Account/Account.js
+++ client/src/Pages/Account/Account.js
@@ -76,15 +76,16 @@ const Account = (props) => {
         </Grid>
         <Grid item xs={6}>
           <Card className={classes.card}>
-            <CardContent>
-              <Link className={classes.link} href="/aboutadmin">
-                <Link className={classes.link} to="/aboutadmin">
-                  <Typography variant="h3" component="span">
-                    About
-                  </Typography>
-                </Link>
-              </Link>
-            </CardContent>
+            <Link className={classes.link} to="/aboutadmin">
+              <CardContent>
+                {/* // TODO Check if first LINK is needed */}
+                {/* <Link className={classes.link} href="/aboutadmin"> */}
+                <Typography variant="h3" component="span">
+                  About
+                </Typography>
+                {/* </Link> */}
+              </CardContent>
+            </Link>
           </Card>
         </Grid>
       </Grid>
