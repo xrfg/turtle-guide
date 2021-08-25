23a193f29106e1bba3fea83a7a7df0496cce9d74
Marco Vignotto <writeme@marcovignotto.com>
2021-08-25 15:27:08 +0200

    Created draft for <AboutAdmin />

--- client/src/Pages/AboutAdmin/AboutAdmin.js
+++ client/src/Pages/AboutAdmin/AboutAdmin.js
@@ -1,18 +1,24 @@
 /*
 ? Component that creates/edit the info of the account (i.e. the museum)
 */
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 // * Mat UI
-import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
-import {
-  Button,
-  Container,
-  Grid,
-  Card,
-  CardContent,
-  Typography,
-} from "@material-ui/core";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+import { Button, Container, Grid, Typography } from "@material-ui/core";
+
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Paper from "@material-ui/core/Paper";
+// import Typography from "@material-ui/core/Typography";
+import ButtonBase from "@material-ui/core/ButtonBase";
+
+// * Pages
+
+// * Components
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -20,6 +26,7 @@ const useStyles = makeStyles((theme) =>
       "& > *": {
         margin: theme.spacing(1),
       },
+      maxWidth: 345,
     },
     mainContainer: {
       display: "flex",
@@ -35,16 +42,16 @@ const useStyles = makeStyles((theme) =>
     input: {
       display: "none",
     },
+    media: {
+      height: 140,
+    },
   })
 );
 
 export default function AboutAdmin() {
   const classes = useStyles();
 
-  // * set array to contain the contents
-  let arrContents = [];
-
-  // * Obj Content
+  // Obj Content
   const objContent = {
     id: 1, // sequential unique id
     type: "text", // it can be whatever
@@ -57,15 +64,118 @@ export default function AboutAdmin() {
 
   // * States
   // state that contains all the contents
-  const [contents, setContents] = useState(arrContents);
+  const [contents, setContents] = useState([]);
 
-  // const [overview, setOverview] = useState([]);
+  // * Functions
+  /**
+   * @function addToContents
+   * @param obj // obj to add
+   * @desc adds a content into the array "arr" and state "contents"
+   */
+
+  const addToContents = (obj) => {
+    setContents([...contents, obj]);
+  };
+
+  // * Card Compenent
+  // TODO Export Component
+  const CardToRender = (props) => {
+    // destru
+    const {
+      id,
+      content: { title, text },
+    } = props.item;
+
+    return (
+      <Paper className={classes.paper}>
+        <Grid container spacing={2}>
+          <Grid item>
+            <ButtonBase className={classes.image}>
+              <img
+                className={classes.img}
+                alt="complex"
+                src="/static/images/grid/complex.jpg"
+              />
+            </ButtonBase>
+          </Grid>
+          <Grid item xs={12} sm container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs>
+                <Typography gutterBottom variant="subtitle1">
+                  {title}
+                </Typography>
+                <Typography variant="body2" gutterBottom>
+                  {text}
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  {id}
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="body2" style={{ cursor: "pointer" }}>
+                  Remove
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <Typography variant="subtitle1">$19.00</Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
+    );
+
+    // return (
+    //   <Card className={classes.root}>
+    //     <CardActionArea>
+    //       <CardMedia
+    //         className={classes.media}
+    //         image="/static/images/cards/contemplative-reptile.jpg"
+    //         title="Contemplative Reptile"
+    //       />
+    //       <CardContent>
+    //         <Typography gutterBottom variant="h5" component="h2">
+    //           {title}
+    //         </Typography>
+    //         <Typography variant="body2" color="textSecondary" component="p">
+    //           {text}
+    //         </Typography>
+    //       </CardContent>
+    //     </CardActionArea>
+    //     <CardActions>
+    //       <Button size="small" color="primary">
+    //         Share
+    //       </Button>
+    //       <Button size="small" color="primary">
+    //         Learn More
+    //       </Button>
+    //     </CardActions>
+    //   </Card>
+    // );
+  };
 
   return (
-    <Container>
-      <h1>customize how visitors will see your guide </h1>
-      <Container maxWidth="xl" className={classes.mainContainer}>
-        <Grid item xs={6} className={classes.btnSection}>
+    <Container maxWidth="sm">
+      {/* // ? Buttons container */}
+      <Grid container spacing={3}>
+        <h1>customize how visitors will see your guide </h1>
+        <Grid item xs={3} className={classes.btnSection}>
+          <Button
+            size="small"
+            variant="contained"
+            color="primary"
+            component="span"
+            onClick={() => addToContents(objContent)}
+          >
+            add Content
+          </Button>
+        </Grid>
+      </Grid>
+
+      {/* // ? Contents container */}
+      <Grid container spacing={3}>
+        <Grid item xs={3} className={classes.btnSection}>
+          {/* 
           <input
             accept="image/*"
             className={classes.input}
@@ -150,14 +260,16 @@ export default function AboutAdmin() {
             >
               add image description
             </Button>
-          </label>
+          </label> */}
         </Grid>
-        <Grid xs={6}>
-          <Card className={classes.overview}>
-            <CardContent>ss</CardContent>
-          </Card>
+        <Grid xs={9}>
+          {!contents
+            ? null
+            : contents.map((x, i) => {
+                return <CardToRender item={x} />;
+              })}
         </Grid>
-      </Container>
+      </Grid>
     </Container>
   );
 }
