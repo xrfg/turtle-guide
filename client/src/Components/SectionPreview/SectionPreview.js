7cb18bec79545955602b265ae223cecde6996348
Marco Vignotto <writeme@marcovignotto.com>
2021-08-26 15:38:34 +0200

    Preview first step

---
+++ client/src/Components/SectionPreview/SectionPreview.js
@@ -0,0 +1,14 @@
+/**
+ * @desc Component that preview the Sections or
+ * renders the sections into the Guide/Event
+ */
+
+import React from "react";
+
+const SectionPreview = (props) => {
+  const { contents } = props;
+  console.log(contents);
+  return <div>SECTION PREVIEW</div>;
+};
+
+export default SectionPreview;
