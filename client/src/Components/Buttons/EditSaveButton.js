3487a65027c6789d2fb02c788c5cfaaa28d61120
henriquemartins <henrm@riseup.net>
2021-08-30 16:09:02 +0200

    trying to make btn save as others, but somehow being a children is not letting me do it

--- client/src/Components/Buttons/EditSaveButton.js
+++ client/src/Components/Buttons/EditSaveButton.js
@@ -1,21 +1,13 @@
 import React from "react";
 
 // * material UI imports Components
-import { Button, makeStyles } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 // * material UI imports Icons
 import { Edit, Save } from "@material-ui/icons";
 
-const useStyles = makeStyles((theme) => ({
-  saveBtn: {
-    height: "100%",
-    alignSelf: "center",
-  },
-}));
-
 export default function EditSaveButton(props) {
-  const classes = useStyles(props);
-
   // * Functions
+
   // * changes the editStatus from "editing" true to "non-editing" false
   const handleSaveEditBtn = (e) => {
     if (props.editStatus === false) {
@@ -26,11 +18,11 @@ export default function EditSaveButton(props) {
   };
   return (
     <Button
-      size={props.size}
+      variant="outlined"
+      size="small"
       onClick={() => {
         handleSaveEditBtn();
       }}
-      className={classes.saveBtn}
       type={props.editStatus ? "submit" : "text"}
       // type is not really working as it should, i.e: not submitting if we click on Edit icon
     >
