ff41459adc9eb5f9eec5ed949403d5a0a175dbfd
henriquemartins <henrm@riseup.net>
2021-09-02 14:28:18 +0200

    refactoring the code and styling

--- client/src/Pages/Account/EventNameInsert.js
+++ client/src/Pages/Account/EventNameInsert.js
@@ -2,39 +2,41 @@
  * @desc Component to insert the name of the event from <Account/>
  */
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import slugify from "react-slugify";
 
 // * material UI imports Icons
-import { TextField, Typography, makeStyles, Box } from "@material-ui/core";
+import { TextField, makeStyles, Button } from "@material-ui/core";
 // * material UI imports Icons
-
-// * React Components
-import EditSaveButton from "../../Components/Buttons/EditSaveButton";
+import { Save } from "@material-ui/icons";
 
 import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
-  nameInput: { width: "24rem", margin: 8 },
+  form: {
+    padding: "1.4rem",
+
+    backgroundColor: "lightgray",
+    borderRadius: "5px",
+    border: "1px solid gray",
+  },
+  nameInput: { marginRight: "1rem" },
   saveBtn: {
     height: "100%",
     alignSelf: "center",
   },
 }));
 
-// TODO insert a modal that opens the event naming if the event does not exists
-
 const EventName = (props) => {
   const classes = useStyles(props);
-  let history = useHistory();
 
-  // * gives the path we're on -> using it to deal with redirect in the Account page
-  const pathName = window.location.pathname;
+  // * Hooks
+
+  // to use history.push(newRoute) on save
+  let history = useHistory();
 
   // * States
   const [eventName, setEventName] = useState("");
-  // true cause it starts with filling the name
-  const [editing, setEditing] = useState(true);
 
   // * Functions
 
@@ -45,27 +47,6 @@ const EventName = (props) => {
 
   const onChange = (e) => {
     setEventName(e.target.value);
-  };
-
-  /**
-   * @function handleSaveEditBtn
-   * @param val boolean
-   * @desc goes into button child component and gets info back wether editing is toggled or not
-   *
-   */
-  const handleSaveEditBtn = (val) => {
-    if (val) {
-      setEditing((prev) => !prev);
-    } else {
-      // TODO REMOVE TOGGLE
-
-      // if we are on the account page, auto redirect to the newly created event
-      // otherwise we are inside of the event page already and just
-      // updating the current name
-      pathName === "/account"
-        ? goToAndSlugify(eventName)
-        : setEditing((prev) => !prev);
-    }
   };
 
   /**
@@ -81,30 +62,35 @@ const EventName = (props) => {
     });
   };
 
+  /**
+   * @function createNewEvent
+   * @desc creates a new event and redirects the user
+   *       to the new event editing page
+   *       while also sending a state with 3 keys (isNew, slug, title)
+   */
+  const createNewEvent = (e) => {
+    e.preventDefault();
+    goToAndSlugify(eventName);
+  };
+
   return (
-    <Box>
-      {editing ? (
-        <TextField
-          disabled={editing ? false : true}
-          id="eventName"
-          type="text"
-          required
-          defaultValue={eventName}
-          placeholder="Name for the Event"
-          helperText="This will be the public name of the Event"
-          className={classes.nameInput}
-          InputLabelProps={{
-            shrink: true,
-          }}
-          onChange={onChange}
-        />
-      ) : (
-        <Typography className={classes.nameInput}>
-          {eventName ? eventName : "Event Name"}
-        </Typography>
-      )}
-      <EditSaveButton editStatus={editing} editHandler={handleSaveEditBtn} />
-    </Box>
+    <form className={classes.form} onSubmit={(e) => createNewEvent(e)}>
+      <TextField
+        id="eventName"
+        type="text"
+        required
+        placeholder="Name for the Event"
+        className={classes.nameInput}
+        InputLabelProps={{
+          shrink: true,
+        }}
+        onChange={onChange}
+      />
+
+      <Button className={classes.saveBtn} type="submit">
+        <Save />
+      </Button>
+    </form>
   );
 };
 
