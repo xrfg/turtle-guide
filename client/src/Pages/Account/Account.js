e31e67dc69b589cbbdcd0990be025b6a4a419895
henriquemartins <henrm@riseup.net>
2021-09-02 14:37:10 +0200

    little code refactoring

--- client/src/Pages/Account/Account.js
+++ client/src/Pages/Account/Account.js
@@ -1,19 +1,18 @@
 /**
  * @desc Home page of the admin part
- * retrives all the account info
+ * retrieves all the account info (events, about)
+ * with -> possibility to go into an event or create a new one
+ * or -> edit the about admin page
  */
 
 import React, { useEffect, useState } from "react";
 
 // * react-router-dom
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 // * REDUX
 import { useSelector, useDispatch } from "react-redux";
 import { eventsFetch } from "../../store/actions/eventsActions";
-
-// * ACTIONS
-import { eventCreate } from "../../store/actions/eventsActions";
 
 // * MAT UI
 import {
@@ -29,17 +28,17 @@ import {
 import { Add } from "@material-ui/icons";
 
 // * Component Imports
-import EventName from "./EventNameInsert";
+import EventNameInsert from "./EventNameInsert";
 
 const useStyles = makeStyles((theme) => ({
   card: {
     marginTop: "10rem",
+    padding: "1rem",
     textAlign: "center",
-    position: "relative",
   },
+  cardActions: { flexDirection: "column" },
   link: { textDecoration: "none", color: "inherit", width: "100%" },
-  btn: { width: "100%" },
-  box: { flexDirection: "column" },
+  /* btn: { width: "100%" }, */
 }));
 
 const Account = (props) => {
@@ -49,9 +48,8 @@ const Account = (props) => {
 
   //* States
   const [isAddingEvent, setIsAddingEvent] = useState(false);
-  const [event, setEvent] = useState();
 
-  // useEffect that check retrives all the account's info
+  // useEffect retrieves all the account's info on landing
   // ? should be the models user and event linked in MONGO
   useEffect(() => {
     dispatch(eventsFetch()); // for user events
@@ -59,43 +57,10 @@ const Account = (props) => {
     //eslint-disable-next-line
   }, []);
 
-  // * add an event on click "+"
+  // "+" onClick function to toggle the TextField <EventNameInsert/>
   const addEvent = () => {
     setIsAddingEvent((prev) => !prev);
   };
-
-  // const createAndSendEvent = (eventName) => {
-  //   console.log("the event name is", eventName);
-
-  //   // ! the event will be save into mongo and the will check the existence
-  //   // ! of the indentifier / slug
-  //   // ! and in case will throw errors
-  //   // TODO create slug with a regex or a library
-
-  //   setEvent({
-  //     title: eventName,
-  //     nameIdentifier: eventName, // fucntion to make the slug
-  //     slug: eventName, // will be the same
-  //     description: "description", // ? is to do?
-  //     sections: [],
-  //     // TODO CHANGE ACCOUNT
-  //     // WILL BET SENT ONCe IS LOGGED IN
-  //     account: "611e5aca56104a1c09f9d13e",
-  //     // ! spread obj
-  //   });
-  // };
-
-  // // fires when the state event is created/updated
-  // useEffect(() => {
-  //   console.log("event", event);
-  //   // if event is empty do not dispatch
-  //   if (!event) {
-  //     return null;
-  //   }
-  //   // dispatch the event to redux
-  //   dispatch(eventCreate(event));
-  //   //eslint-disable-next-line
-  // }, [event]);
 
   return (
     <Container>
@@ -113,17 +78,15 @@ const Account = (props) => {
                 Events
               </Typography>
             </CardContent>
-            <CardActions className={classes.box}>
+            <CardActions className={classes.cardActions}>
               {events &&
-                events.map((event, i) => {
+                events.map((event) => {
                   return (
                     <Button
                       className={classes.btn}
                       variant="text"
                       color="primary"
                     >
-                      {/* // ! go to section <Event id={EVENT ID from mongo array}/> giving the id   */}
-                      {/* // ! send the obj <Event id={events[i]} */}
                       <Link
                         className={classes.link}
                         to={`/admin/event/${event.slug}`}
@@ -135,9 +98,8 @@ const Account = (props) => {
                 })}
 
               {isAddingEvent ? (
-                <EventName />
+                <EventNameInsert />
               ) : (
-                // <EventName getEventName={createAndSendEvent} />
                 <Button
                   className={classes.btn}
                   variant="text"
@@ -154,12 +116,9 @@ const Account = (props) => {
           <Card className={classes.card}>
             <Link className={classes.link} to="/aboutadmin">
               <CardContent>
-                {/* // TODO Check if first LINK is needed or not */}
-                {/* <Link className={classes.link} href="/aboutadmin"> */}
                 <Typography variant="h3" component="span">
                   About
                 </Typography>
-                {/* </Link> */}
               </CardContent>
             </Link>
           </Card>
