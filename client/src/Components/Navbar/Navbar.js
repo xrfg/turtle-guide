8f434aec22daa98ad49120e430aa6658a88623e5
henriquemartins <henrm@riseup.net>
2021-09-08 11:13:13 +0200

    minimal styling on Navbar and adding the Logo

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -4,27 +4,34 @@
  */
 
 import React from "react";
-import { NavLink, Link, useHistory } from "react-router-dom";
-import "./navbar.scss";
 
-// Imports
+// * react-router-dom Imports
+import { Link } from "react-router-dom";
+
+// * Material UI Imports
+import { AppBar, Toolbar } from "@material-ui/core";
+
+import logo from "../../assets/images/logo/turtleguidelogo.png";
+
+// * Component Imports
 import Menu from "../Menu/Menu";
 
-const Navbar = ({ user }) => {
-  const arrMenu = [
-    { title: "", className: "dropdown-item", to: "/create-event" },
-  ];
-
+const Navbar = () => {
   return (
-    <nav class="navbar">
-      {/* LOGO Clickable */}
-      <div className="navbar__container">
+    <AppBar style={{ backgroundColor: "#D9D9D9" }}>
+      <Toolbar style={{ display: "flex", justifyContent: "space-between" }}>
         <Link className="navbar-brand" to="/">
-          Turtle Guide
+          <img
+            style={{ height: "60px", margin: "0.4rem 0" }}
+            src={logo}
+            alt="logo"
+          />
         </Link>
+
+        {/* has the YOUR ACCOUNT and LOG IN/OUT */}
         <Menu />
-      </div>
-    </nav>
+      </Toolbar>
+    </AppBar>
   );
 };
 
