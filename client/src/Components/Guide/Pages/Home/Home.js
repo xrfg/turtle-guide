b7a940ce8416b4d99f28ae5fc3f9a3226bcbde03
henriquemartins <henrm@riseup.net>
2021-09-08 16:16:46 +0200

    adding styles and fixed bottomnavbar to the guide

--- client/src/Components/Guide/Pages/Home/Home.js
+++ client/src/Components/Guide/Pages/Home/Home.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 
 // * Components
-import BottomNavBar from "../../Components/Navbar/BottomNavBar";
 import { Link, useHistory } from "react-router-dom";
 
 // * Components
@@ -9,23 +8,10 @@ import BlockSection from "../../Components/BlockSection/BlockSection";
 
 // * MatUI
 import { makeStyles } from "@material-ui/core/styles";
-import CardActionArea from "@material-ui/core/CardActionArea";
 import clsx from "clsx";
-import { Grid, Container, Button } from "@material-ui/core";
-import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
-import CardMedia from "@material-ui/core/CardMedia";
-import CardContent from "@material-ui/core/CardContent";
-import CardActions from "@material-ui/core/CardActions";
-import Collapse from "@material-ui/core/Collapse";
-import Avatar from "@material-ui/core/Avatar";
-import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
+import { Container } from "@material-ui/core";
+
 import { red } from "@material-ui/core/colors";
-import FavoriteIcon from "@material-ui/icons/Favorite";
-import ShareIcon from "@material-ui/icons/Share";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -78,10 +64,6 @@ export default function Home(props) {
         {sections.map((x) => {
           return <BlockSection eventSlug={eventSlug} data={x} />;
         })}
-      </Container>
-
-      <Container>
-        <BottomNavBar />
       </Container>
     </>
   );
