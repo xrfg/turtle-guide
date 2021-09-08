dc99d812e5f4e3f24a75e0a6f2cc4e4265cff4f2
Marco Vignotto <writeme@marcovignotto.com>
2021-09-08 14:02:38 +0200

    Upload/Change cover image, ok. Created <ImageHoverButton/>

--- client/src/Components/SectionContentManager/SectionContentManager.js
+++ client/src/Components/SectionContentManager/SectionContentManager.js
@@ -45,6 +45,7 @@ import { eventUpdate } from "../../store/actions/eventsActions";
 
 // * Functions
 import { goBackToPage, unBlock } from "../../functions/functions";
+import ImageHoverButton from "../Buttons/ImageHoverButton";
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -89,76 +90,6 @@ const useStyles = makeStyles((theme) =>
     },
     media: {
       height: 140,
-    },
-
-    // * Hover buttons
-    image: {
-      position: "relative",
-      height: 200,
-      [theme.breakpoints.down("xs")]: {
-        width: "100% !important", // Overrides inline-style
-        height: 100,
-      },
-      "&:hover, &$focusVisible": {
-        zIndex: 1,
-        "& $imageBackdrop": {
-          opacity: 0.4,
-        },
-        "& $imageMarked": {
-          opacity: 0,
-        },
-        "& $imageTitle": {
-          visibility: "visible", // show title on hover
-          // border: "4px solid currentColor",
-        },
-      },
-    },
-    focusVisible: {},
-    imageButton: {
-      position: "absolute",
-      left: 0,
-      right: 0,
-      top: 0,
-      bottom: 0,
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      color: theme.palette.common.white,
-    },
-    imageSrc: {
-      position: "absolute",
-      left: 0,
-      right: 0,
-      top: 0,
-      bottom: 0,
-      backgroundSize: "cover",
-      backgroundPosition: "center 40%",
-    },
-    imageBackdrop: {
-      position: "absolute",
-      left: 0,
-      right: 0,
-      top: 0,
-      bottom: 0,
-      backgroundColor: theme.palette.common.black,
-      opacity: 0,
-      transition: theme.transitions.create("opacity"),
-    },
-    imageTitle: {
-      visibility: "hidden", // show title on hover
-      position: "relative",
-      padding: `${theme.spacing(2)}px ${theme.spacing(4)}px ${
-        theme.spacing(1) + 6
-      }px`,
-    },
-    imageMarked: {
-      // height: 3,
-      // width: 18,
-      backgroundColor: theme.palette.common.white,
-      position: "absolute",
-      bottom: -2,
-      left: "calc(50% - 9px)",
-      transition: theme.transitions.create("opacity"),
     },
   })
 );
@@ -569,6 +500,19 @@ export default function SectionContentManager(props) {
     console.log(newContentsState);
   };
 
+  /**
+   * @function addCoverImage
+   * @param noParam
+   * @desc fires the adding of a cover image
+   */
+
+  const addCoverImage = () => {
+    // set to true to add the image
+    // this will call a condition to set the proper key into obj section
+    isAddingCover = true;
+    showCloudinaryWidget(cloudinaryWidget);
+  };
+
   // * Objects
   /**
    * @desc obj for the section description
@@ -616,45 +560,11 @@ export default function SectionContentManager(props) {
                 Add a cover Image
               </Button>
             ) : (
-              <ButtonBase
-                focusRipple
-                key={section.sectionCover.filename}
-                className={classes.image}
-                focusVisibleClassName={classes.focusVisible}
-                style={{
-                  width: "900px", // ! decide the size of the cover
-                }}
-                onClick={() => {
-                  isAddingCover = true;
-                  showCloudinaryWidget(cloudinaryWidget);
-                }}
-              >
-                <span
-                  className={classes.imageSrc}
-                  style={{
-                    backgroundImage: `url(${section.sectionCover.url})`,
-                  }}
-                />
-                <span className={classes.imageBackdrop} />
-                <span className={classes.imageButton}>
-                  <Typography
-                    component="span"
-                    variant="subtitle1"
-                    color="inherit"
-                    className={classes.imageTitle}
-                  >
-                    Change Cover Image
-                    <span className={classes.imageMarked} />
-                  </Typography>
-                </span>
-              </ButtonBase>
-
-              // // show img
-              // <img
-              //   className={classes.sectionCover}
-              //   alt="section-cover"
-              //   src={section.sectionCover.url}
-              // />
+              <ImageHoverButton
+                title={"Change Cover Image"}
+                onClickFunc={addCoverImage}
+                image={section.sectionCover}
+              />
             )}
 
             {/* Section description Edit */}
