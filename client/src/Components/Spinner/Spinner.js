3d775a6f68f36d279bb1eed25fc1c2e00eea5ca9
henriquemartins <henrm@riseup.net>
2021-09-28 16:18:59 +0200

    change css spinner, blue and centered

--- client/src/Components/Spinner/Spinner.js
+++ client/src/Components/Spinner/Spinner.js
@@ -6,20 +6,30 @@ import React, { useState } from "react";
 
 import { css } from "@emotion/react";
 import ClipLoader from "react-spinners/ClipLoader";
+import { ourColors } from "../../styles/Theme";
 
 // Can be a string as well. Need to ensure each key-value pair ends with ;
 const override = css`
   display: block;
   margin: 0 auto;
-  border-color: red;
+  border-color: ${ourColors.indigoDye};
 `;
 
 const Spinner = () => {
   let [loading] = useState(true);
-  let [color] = useState("red");
+  let [color] = useState(`${ourColors.indigoDye}`);
 
   return (
-    <ClipLoader color={color} loading={loading} css={override} size={150} />
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "calc(100vh - 90px)",
+      }}
+    >
+      <ClipLoader color={color} loading={loading} css={override} size={150} />
+    </div>
   );
 };
 
